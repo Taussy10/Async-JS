ba4ec6f7372cffe9531ae221857b27b23f5175d3
Tausif <tausif00100@gmail.com>
2025-05-04 15:58:27 +0530

    learned about JSON

---
+++ 01_what-is-api/script.js
@@ -0,0 +1,23 @@
+// We can write code for two things using JS in context of web development
+// 1. For browser: basic JS(loops, data types) and DOM related things
+// 2. What if we want to interact with servers(A place where data is stored)
+// for data sending and recieving  So, how to interact with servers
+
+// When we search something on google then a request is send to google's server 
+// then server sends the data and we see pages links
+// you can see in chrome dev tools's Network tab by refreshing it
+// What's request: Electro maganetic signals that tells to do something  
+
+// JSON: JS Object Notation: A lightweight human readable data interchange format
+// 1. even keys can be written in quotes and value same as js objects(if num then num) 
+// if you use ,(comma) then write another key and value 
+
+// WHy ? 
+// 1. For storing data 
+// 2. Transmitting data from server to client to server 
+
+// Note: it's not language independent
+
+// There ar two ways 
+// 1. data.json() will convert data in JSON format the in JS object(useful for API fetching)
+// 1. data.json() will convert JSON in JS Object (useful if data is already in JSON: AI realated work)
